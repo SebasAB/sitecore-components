46865ecc6d2a13fabbceac3192d547e7c896dc9a
sebasab <sebasaliaga2515@gmail.com>
2024-02-21 21:34:46 -0500

    fixed identation

--- src/commandsComponents/componentWithPlaceholder.ts
+++ src/commandsComponents/componentWithPlaceholder.ts
@@ -1,52 +1,52 @@
 import * as vscode from "vscode";
 
 export const componentWithPlaceholder = () => {
-    let componentWithPlaceholder = vscode.commands.registerCommand(
-        "sitecore-xm-cloud-components.insertComponentWithPlaceholder",
-        () => {
-          const editor = vscode.window.activeTextEditor;
-          if (editor) {
-            const filename = editor.document.fileName.split(/[/\\]/).pop();
-            if (filename) {
-              const componentName = filename.replace(/\.[^/.]+$/, "");
-              const propsTypeName = `${componentName}Props`;
-    
-              const componentTemplate = `import { Field, withDatasourceCheck, Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
-    import { Text } from '@sitecore-jss/sitecore-jss-react';
-    import { ComponentProps } from 'lib/component-props';
-    
-    interface Fields {
-        // Define your fields here
-        heading: Field<string>;
-        $1
-    }
-    
-    type ${propsTypeName} = ComponentProps & {
-        fields: Fields;
-    };
-    
-    const ${componentName} = ({ fields, params, rendering }: ${propsTypeName}) => {
-        return (
-            <section className={params.styles}>
-                {/* Component content */}
-                <Text field={fields.heading} />
-                <Placeholder name="$2" rendering={rendering} />
-                $3
-            </section>
-        );
-    };
-    
-    export default withDatasourceCheck()<${propsTypeName}>(${componentName});`;
-    
-              editor.insertSnippet(new vscode.SnippetString(componentTemplate));
-            } else {
-              vscode.window.showErrorMessage("Cannot determine the filename.");
-            }
-          } else {
-            vscode.window.showErrorMessage("Editor is not active.");
-          }
-        }
-      )
+  let componentWithPlaceholder = vscode.commands.registerCommand(
+    "sitecore-xm-cloud-components.insertComponentWithPlaceholder",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const filename = editor.document.fileName.split(/[/\\]/).pop();
+        if (filename) {
+          const componentName = filename.replace(/\.[^/.]+$/, "");
+          const propsTypeName = `${componentName}Props`;
 
-      return componentWithPlaceholder
+          const componentTemplate = `import { Field, withDatasourceCheck, Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Text } from '@sitecore-jss/sitecore-jss-react';
+import { ComponentProps } from 'lib/component-props';
+    
+interface Fields {
+    // Define your fields here
+    heading: Field<string>;
+    $1
 }
+  
+type ${propsTypeName} = ComponentProps & {
+    fields: Fields;
+};
+    
+const ${componentName} = ({ fields, params, rendering }: ${propsTypeName}) => {
+    return (
+        <section className={params.styles}>
+            {/* Component content */}
+            <Text field={fields.heading} />
+            <Placeholder name="$2" rendering={rendering} />
+            $3
+        </section>
+    );
+};
+    
+export default withDatasourceCheck()<${propsTypeName}>(${componentName});`;
+
+          editor.insertSnippet(new vscode.SnippetString(componentTemplate));
+        } else {
+          vscode.window.showErrorMessage("Cannot determine the filename.");
+        }
+      } else {
+        vscode.window.showErrorMessage("Editor is not active.");
+      }
+    }
+  );
+
+  return componentWithPlaceholder;
+};
